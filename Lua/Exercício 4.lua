0475b295c34e32dfe5fa3ddf07ab7c63f5dd81e4
Mayara Cristina <mayara.c@ges.inatel.br>
2025-09-09 16:02:16 -0300

    Exercício 4.lua

--- Lua/Exercício 4.lua
+++ Lua/Exercício 4.lua
@@ -1,20 +1,21 @@
-print("Digite seu primeiro numero: ")
-local n1 = tonumber(io.read())
-print("Digite seu segundo numero: ")
-local n2 = tonumber(io.read())
+print("Digite o primeiro número: ")
+local valor1 = tonumber(io.read())
+print("Digite o segundo número: ")
+local valor2 = tonumber(io.read())
 
-print("Digite um operador matemático: ")
-local op = tonumber(io.read())
+print("Digite um operador matemático (+, -, *, /): ")
+local operador = io.read()
 
-function calculadora(n1, n2, op)
-    if op == "+" then
-        print(n1 + n2)
-    elseif op == "-" then
-        print(n1 - n2)
-    elseif op == "*" then
-        print(n1 * n2)
-    elseif op == "/" then
-        print(n1 / n2)
+function calcular(numA, numB, simbolo)
+    if simbolo == "+" then
+        print(numA + numB)
+    elseif simbolo == "-" then
+        print(numA - numB)
+    elseif simbolo == "*" then
+        print(numA * numB)
+    elseif simbolo == "/" then
+        print(numA / numB)
     end
-        
 end
+
+calcular(valor1, valor2, operador)
