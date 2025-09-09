a47b1478762338241350cadf52a41f3ee1ebaf3f
Mayara Cristina <mayara.c@ges.inatel.br>
2025-09-09 15:58:36 -0300

    Exercício 1.lua

--- Lua/Exercício 1.lua
+++ Lua/Exercício 1.lua
@@ -1,13 +1,14 @@
-print("Digite seu primeiro numero: ")
-local m = tonumber(io.read())
-print("Digite seu segundo numero: ")
-local n = tonumber(io.read())
+print("Digite o valor inicial: ")
+local inicio = tonumber(io.read())
 
-print("Digite o número que você deseja saber o múltiplo: ")
-local x = tonumber(io.read())
+print("Digite o valor final: ")
+local fim = tonumber(io.read())
 
-for i = m, n, 1 do --start, end, step
-  if i % x == 0 then
-    print(i)
+print("Digite o número para verificar os múltiplos: ")
+local divisor = tonumber(io.read())
+
+for numero = inicio, fim, 1 do --start, end, step
+  if numero % divisor == 0 then
+    print(numero)
   end
 end
