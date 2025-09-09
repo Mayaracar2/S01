d02edf9acbd066bd7c6b94741b35ff05dc800b3b
Mayara Cristina <mayara.c@ges.inatel.br>
2025-09-09 16:00:01 -0300

    Exercício 2.lua

--- Lua/Exercício 2.lua
+++ Lua/Exercício 2.lua
@@ -1,23 +1,23 @@
-local numeros = {}
-local valor
-local cont = 0
+local lista = {}
+local entrada
+local quantidade = 0
 
 print("Digite números (digite 0 para parar): ")
 
 while true do
-    valor = tonumber(io.read())
-    if valor == 0 then --coloquei uma condição de parada para dar um stop na leitura do                           numeros
+    entrada = tonumber(io.read())
+    if entrada == 0 then -- condição de parada
         break
     end
-    table.insert(numeros, valor)
-    cont = cont + 1
+    table.insert(lista, entrada)
+    quantidade = quantidade + 1
 end
 
-local maior = numeros[1] --Afirma que o maior numero é o primeiro termo
-for i = 2, cont do       --por isso começa a contar a partir do 2
-    if numeros[i] > maior then
-        maior = numeros[i]
+local maximo = lista[1] -- assume que o primeiro é o maior
+for indice = 2, quantidade do
+    if lista[indice] > maximo then
+        maximo = lista[indice]
     end
 end
 
-print("O maior número é: " .. maior)
+print("O maior número é: " .. maximo)
