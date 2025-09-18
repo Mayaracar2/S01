05d97ab220a68a2db257eb0f2f0ef2ee3214a63e
Mayara Cristina <mayara.c@ges.inatel.br>
2025-09-18 20:10:56 -0300

    Exercício 2.cpp

---
+++ C++/Exercício 2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+float somarArray(float arr[], int t)
+{
+	int soma = 0;
+	
+	for(int i = 0; i < t; i++)
+	{	
+		soma += arr[i];
+	}
+	return(soma);
+}
+int main(){
+	
+	int t;
+	float arr[100];
+	
+	cout << "Insira a quantidade de numeros: ";
+	cin >> t;
+	
+	for(int i = 0; i < t; i++)
+	{	
+		cin >> arr[i];
+	}
+	
+	cout << "Soma: " << somarArray(arr,t);
+	
+	return 0;
+}
