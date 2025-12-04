fcd4d710621f6d6ce0235bcdded85155ed2ae154
Mayara Cristina <mayara.c@ges.inatel.br>
2025-12-04 20:05:19 -0300

    Update Exercício 3.pl
    Updated disciplines and prerequisites for students.

--- ProLog Parte 2/Exercício 3.pl
+++ ProLog Parte 2/Exercício 3.pl
@@ -1,47 +1,48 @@
-% Fatos: Disciplinas (disciplinas, créditos, area)
-disciplina(base_de_programação, 3, fundamental).
-disciplina(calculo, 7, matematica).
-disciplina(controle_de_dados, 6, computacao).
-disciplina(paradigmas, 4, computacao).
-disciplina(banco_de_dados, 6, aplicacoes).
+% Fatos: Disciplinas (nome, créditos, area)
+disciplina(programacao1, 6, fundamental).
+disciplina(calculo1, 8, matematica).
+disciplina(estrutura_dados, 6, computacao).
+disciplina(algoritmos, 4, computacao).
+disciplina(banco_dados, 6, aplicacoes).
 disciplina(redes, 4, aplicacoes).
 disciplina(inteligencia_artificial, 8, computacao).
 disciplina(compiladores, 6, computacao).
-disciplina(teorema_dos_graficos, 6, matematica).
-disciplina(controle_de_dados, 6, matematica).
+disciplina(teoria_grafos, 4, matematica).
+disciplina(estatistica, 6, matematica).
 disciplina(sistemas_operacionais, 6, computacao).
 disciplina(projeto_integrador, 10, aplicacoes).
 
 % Fatos: Pré-Requisitos (disciplina_principal, pre_requisito)
-prerequisito(controle_de_dados, base_de_programação).
-prerequisito(algoritmos, controle_de_dados).
-prerequisito(banco_de_dados, base_de_programação).
+prerequisito(estrutura_dados, programacao1).
+prerequisito(algoritmos, estrutura_dados).
+prerequisito(banco_dados, programacao1).
 prerequisito(inteligencia_artificial, algoritmos).
-prerequisito(inteligencia_artificial, calculo).
+prerequisito(inteligencia_artificial, calculo1).
 prerequisito(compiladores, algoritmos).
-prerequisito(compiladores, controle_de_dados).
-prerequisito(sistemas_operacionais, controle_de_dados).
-prerequisito(teorema_dos_graficos, calculo).
-prerequisito(paradigmas, calculo).
-prerequisito(projeto_integrador, banco_de_dados).
+prerequisito(compiladores, estrutura_dados).
+prerequisito(sistemas_operacionais, estrutura_dados).
+prerequisito(teoria_grafos, calculo1).
+prerequisito(estatistica, calculo1).
+prerequisito(projeto_integrador, banco_dados).
 prerequisito(projeto_integrador, sistemas_operacionais).
 
 % Fatos: Conhecimento de Alunos (aluno, disciplina_concluida)
-concluiu(maria, base_de_programação).
-concluiu(maria, calculo).
-concluiu(maria, controle_de_dados).
-concluiu(joao, base_de_programação).
-concluiu(joao, controle_de_dados).
-concluiu(joao, calculo).
-concluiu(jose, base_de_programação).
-concluiu(jose, redes).
-concluiu(jose, teorema_dos_graficos).
-concluiu(paulo, calculo).
-concluiu(paulo, paradigmas).
-concluiu(paulo, teorema_dos_graficos).
-concluiu(rogerio, controle_de_dados).
-concluiu(rogerio, algoritmos).
-concluiu(rogerio, redes). 
+% Configuração de Alunos para testes:
+concluiu(maria, programacao1).
+concluiu(maria, calculo1).
+concluiu(joao, programacao1).
+concluiu(joao, estrutura_dados).
+concluiu(joao, calculo1).
+concluiu(ana, programacao1).
+concluiu(ana, redes).
+concluiu(ana, teoria_grafos).
+concluiu(pedro, calculo1).
+concluiu(pedro, estatistica).
+concluiu(pedro, teoria_grafos).
+concluiu(lucas, estrutura_dados).
+concluiu(lucas, algoritmos).
+concluiu(lucas, redes). 
+concluiu(lucas, banco_dados).
 
 especialista_comp(Aluno) :-
 
@@ -58,7 +59,6 @@ especialista_comp(Aluno) :-
 deficiencia_mat(Aluno) :-
 
     \+ (concluiu(Aluno, D),
-    
-    disciplina(D, _, matematica)).
-    
+        disciplina(D, _, matematica)).
+
 % Query: % ?- especialista_comp(A), deficiencia_mat(A).
